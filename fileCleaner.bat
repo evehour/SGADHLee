b9af16a392d6c0d889a1f320dffc3d09dc232784
evehour <evehour@naver.com>
2018-08-07 17:53:22 +0900

    test

--- fileCleaner.bat
+++ fileCleaner.bat
@@ -2,5 +2,5 @@ del /f /q /s /a rhsa *.bak *.ncb *.obj *.pdb *.idb *.ilk *.pch *.suo *.aps *.opt
 for /f "tokens=*" %%I in ('dir /b /s /ad .vs*') do (rmdir /s /q "%%I")
 for /f "tokens=*" %%I in ('dir /b /s /ad Debug*') do (rmdir /s /q "%%I")
 for /f "tokens=*" %%I in ('dir /b /s /ad release*') do (rmdir /s /q "%%I")
-for /f "tokens=*" %%I in ('dir /b /s /ad x64*') do (rmdir /s /q "%%I")
+for /f "tokens=*" %%I in ('dir /b /s /ad x641*') do (rmdir /s /q "%%I")
 pause
