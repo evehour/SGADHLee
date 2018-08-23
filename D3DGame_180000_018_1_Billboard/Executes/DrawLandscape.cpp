699c5007c282c09865bf500dd1ca42cd4429ae3c
evehour <evehour@naver.com>
2018-08-24 03:18:41 +0900

    *day18::HW ready

---
+++ D3DGame_180000_018_1_Billboard/Executes/DrawLandscape.cpp
@@ -0,0 +1,129 @@
+#include "stdafx.h"
+#include "DrawLandscape.h"
+#include "../Landscape/Sky.h"
+#include "../Landscape/Terrain.h"
+#include "../Objects/MeshSphere.h"
+#include "../Objects/Billboard.h"
+
+DrawLandscape::DrawLandscape(ExecuteValues * values)
+	: Execute(values)
+{
+	sky = new Sky(values);
+
+	//Create Terrain
+	{
+		wstring heightMap = Contents + L"HeightMaps/HeightMap256.png";
+		Material* material = new Material(Shaders + L"022_TerrainBrushLine.hlsl");
+		
+		terrain = new Terrain(values, material);
+	}
+
+	//Create MeshSphere
+	{
+		sphere = new MeshSphere();
+
+		D3DXMATRIX S;
+		D3DXMatrixScaling(&S, 3, 3, 3);
+		sphere->RootAxis(S);
+
+		sphere->Position(D3DXVECTOR3(80, 10, 30));
+	}
+
+	//Create Trees
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			Billboard* tree = new Billboard(values, Shaders + L"024_Billboard.hlsl", Textures + L"Tree.png");
+			D3DXVECTOR3 P;
+			P.x = Math::Random(0.0f, 255.0f);
+			P.z = Math::Random(0.0f, 255.0f);
+			P.y = terrain->Y(P);
+
+			D3DXVECTOR3 S;
+			S.x = Math::Random(10.0f, 30.0f);
+			S.y = Math::Random(10.0f, 30.0f);
+			S.z = 1;
+
+			bool fixedY = Math::Random(0, 1) == 0 ? false : true;
+			tree->FixedY(fixedY);
+
+			P.y += S.y * 0.5f;
+
+			tree->Position(P);
+			tree->Scale(S);
+			
+			trees.push_back(tree);
+		}
+	}
+}
+
+DrawLandscape::~DrawLandscape()
+{
+	for (Billboard* tree : trees)
+		SAFE_DELETE(tree);
+
+	SAFE_DELETE(sphere);
+	SAFE_DELETE(sky);
+	SAFE_DELETE(terrain);
+}
+
+void DrawLandscape::Update()
+{
+	D3DXVECTOR3 position = sphere->Position();
+	
+	if (Keyboard::Get()->Press('I'))
+		position.z += 10.0f * Time::Delta();
+	else if (Keyboard::Get()->Press('K'))
+		position.z -= 10.0f * Time::Delta();
+
+	if (Keyboard::Get()->Press('J'))
+		position.x -= 10.0f * Time::Delta();
+	else if (Keyboard::Get()->Press('L'))
+		position.x += 10.0f * Time::Delta();
+
+	//position.y = terrain->Y(position);
+	D3DXVECTOR3 newPosition;
+	if (terrain->Y(&newPosition, position) == true)
+		position.y = newPosition.y;
+
+	sphere->Position(position);
+
+	sky->Update();
+	terrain->Update();
+	sphere->Update();
+
+	for (Billboard* tree : trees)
+		tree->Update();
+}
+
+void DrawLandscape::PreRender()
+{
+
+}
+
+void DrawLandscape::Render()
+{
+	sky->Render(); // 젤먼저 그려야 함.
+
+	terrain->Render();
+	sphere->Render();
+
+	for (Billboard* tree : trees)
+		tree->Render();
+}
+
+void DrawLandscape::PostRender()
+{
+	if (terrain->GetBrushBuffer()->Data.Type > 0)
+	{
+		D3DXVECTOR3 picked;
+		terrain->Y(&picked);
+
+		ImGui::LabelText("Picked", "%.2f, %.2f, %.2f", picked.x, picked.y, picked.z);
+	}
+}
+
+void DrawLandscape::ResizeScreen()
+{
+}
+
---
+++ D3DGame_180000_018_1_Billboard/Landscape/Terrain.h
@@ -0,0 +1,95 @@
+#pragma once
+
+class Terrain
+{
+public:
+	Terrain(ExecuteValues* values, Material* material);
+	~Terrain();
+
+	void Update();
+	void Render();
+
+	float Y(D3DXVECTOR3& position);
+	bool Y(OUT D3DXVECTOR3* out, D3DXVECTOR3& position);
+	bool Y(OUT D3DXVECTOR3* out);
+	void AdjustY(D3DXVECTOR3& location);
+
+private:
+	void CreateData();
+	void CreateNormalData();
+	void CreateBuffer();
+
+private:
+	ExecuteValues* values;
+
+	Material* material;
+	WorldBuffer* worldBuffer;
+
+	Texture* heightTexture;
+	Texture* colorTexture;
+	Texture* colorTexture2;
+	Texture* alphaTexture;
+
+	UINT width, height;
+
+	VertexTextureNormal* vertices;
+	UINT* indices;
+
+	UINT vertexCount, indexCount;
+
+	ID3D11Buffer* vertexBuffer, *indexBuffer;
+
+	bool bWireFrame;
+	RasterizerState* rasterizer[2];
+
+private:
+	class BrushBuffer : public ShaderBuffer
+	{
+	public:
+		BrushBuffer() : ShaderBuffer(&Data, sizeof(Data))
+		{
+			Data.Type = 0;
+			Data.Location = D3DXVECTOR3(0, 0, 0);
+			Data.Range = 2;
+			Data.Color = D3DXVECTOR3(0, 1, 0);
+		}
+
+		struct Struct
+		{
+			int Type;
+			D3DXVECTOR3 Location;
+
+			int Range;
+			D3DXVECTOR3 Color;
+		} Data;
+	};
+	BrushBuffer* brushBuffer;
+
+	class GridBuffer : public ShaderBuffer
+	{
+	public:
+		GridBuffer() : ShaderBuffer(&Data, sizeof(Data))
+		{
+			Data.Type = 1;
+			Data.Color = D3DXVECTOR3(1, 1, 1);
+
+			Data.Spacing = 1;
+			Data.Thickness = 0.1f;
+		}
+
+		struct Struct
+		{
+			int Type;
+			D3DXVECTOR3 Color;
+
+			int Spacing;
+			float Thickness;
+
+			float Padding[2];
+		} Data;
+	};
+	GridBuffer* gridBuffer;
+
+public:
+	BrushBuffer* GetBrushBuffer() { return brushBuffer; }
+};
