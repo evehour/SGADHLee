ee35ea3168414b8a6537add6593bd2f567bbed11
evehour <evehour@gmail.com>
2018-10-30 17:26:35 +0900

    day60::split_05

---
+++ D3DGame_180000_052_1_Atmosphere_Scattering/Lights/Shadow.h
@@ -0,0 +1,58 @@
+#pragma once
+class Shadow
+{
+public:
+	// 투영영역
+	//const float Width = 2048;
+	//const float Height = 2048;
+
+public:
+	Shadow(ExecuteValues* values);
+	~Shadow();
+
+	void Add(class GameModel* obj);
+
+	void Update();
+	void PreRender();
+	void Render();
+
+private:
+	ExecuteValues* values;
+
+	Shader* shaderDepth;
+	RenderTarget* depthShadow;
+	vector<class GameModel*> objs;
+	vector<class GameModel*> renderObjs;
+
+	Shader * shaderRender;
+
+	class Render2D* depthRender;
+	class Perspective* perspective;
+	ViewProjectionBuffer* vpBuffer;
+
+	SamplerState* samplerState[2];
+
+	D3D11_SAMPLER_DESC desc;
+
+	ID3D11SamplerState* state;
+
+private:
+	class Buffer : public ShaderBuffer
+	{
+	public:
+		Buffer() : ShaderBuffer(&Data, sizeof(Data))
+		{
+			Data.Bias = 0.0005f;
+			Data.Selected = 0;
+		}
+
+		struct Struct
+		{
+			D3DXVECTOR2 Size;
+			float Bias;
+			int Selected;
+		} Data;
+	};
+
+	Buffer* buffer;
+};
